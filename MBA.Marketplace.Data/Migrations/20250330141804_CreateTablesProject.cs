82ff6ff082e96625bbb41ebbf6dce5444d557e12
Igor Vettorello <igor.vettorello@hotmail.com>
2025-04-04 17:27:50 -0300

    fix: correção para executar o projeto

---
+++ MBA.Marketplace.Data/Migrations/20250330141804_CreateTablesProject.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MBA.Marketplace.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTablesProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendedores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    UsuarioId = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vendedores_AspNetUsers_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Imagem = table.Column<string>(type: "TEXT", nullable: false),
+                    preco = table.Column<decimal>(type: "TEXT", nullable: false),
+                    estoque = table.Column<int>(type: "INTEGER", nullable: false),
+                    CategoriaId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    VendedorId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Vendedores_VendedorId",
+                        column: x => x.VendedorId,
+                        principalTable: "Vendedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_VendedorId",
+                table: "Produtos",
+                column: "VendedorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendedores_UsuarioId",
+                table: "Vendedores",
+                column: "UsuarioId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "Vendedores");
+        }
+    }
+}
---
+++ MBA.Marketplace.Data/Migrations/20250331214648_AjustesCampoProject.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MBA.Marketplace.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustesCampoProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "preco",
+                table: "Produtos",
+                newName: "Preco");
+
+            migrationBuilder.RenameColumn(
+                name: "estoque",
+                table: "Produtos",
+                newName: "Estoque");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Vendedores",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Vendedores");
+
+            migrationBuilder.RenameColumn(
+                name: "Preco",
+                table: "Produtos",
+                newName: "preco");
+
+            migrationBuilder.RenameColumn(
+                name: "Estoque",
+                table: "Produtos",
+                newName: "estoque");
+        }
+    }
+}
